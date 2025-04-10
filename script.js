6347678da2e8216f4613f7822555f5ac2c5531c1
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-10 12:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 const makeChange = (c) => {
   // your name here
+	const money = [
+		["q", 25],
+		["d", 10],
+		["n", 5],
+		["p", 1],
+	]
+	// let result = {
+		let q =0,
+		let d =0,
+		let n =0,
+		let p =0,
+		// d:0,
+		// n:0,
+		// p:0,
+	// }
+
+	for(let[symbol, value] of money){
+		while (c>=value) {
+			if(symbol==q){
+				q++;
+			}
+			else if(symbol == d){
+				d++;
+			}
+			else if(symbol ==n){
+				n++;
+			}
+			else if(symbol ==p){
+				p++;
+			}
+			c -= value;
+					
+		}
+	}
+	let result = `q:${q} d:${d} n:${n} p:${p} `
+	return result;
 };
 
-// Do not the change the code below
+// Do not the change the code belocw
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
